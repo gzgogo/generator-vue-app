4d5c34e187bcf6ec72e2f961b2db596a00a74b00
gongzhen <gongzhen.gz@alibaba-inc.com>
2016-07-10 11:38:11 +0800

    enhance default component

--- generators/vue-component/templates/component.vue
+++ generators/vue-component/templates/component.vue
@@ -4,9 +4,36 @@
 
 <template>
   <div class="<%= componentName %>">
+    <hello-vue></hello-vue>
+    <button type="button" @click="sayHello">{{name}}</button>
   </div>
 </template>
 
 <script>
+  import helloVue from '../hello-vue/hello-vue.vue';
 
+  export default {
+    data () {
+      return {
+        name: 'Lucy'
+      }
+    },
+
+    compiled () {
+
+    },
+
+    ready () {
+    },
+
+    methods: {
+      sayHello() {
+        alert('Hello ' + this.name);
+      }
+    },
+
+    components: {
+      helloVue: helloVue
+    }
+  }
 </script>
